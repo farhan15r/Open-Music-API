b6251fd0c0b487a7fa370a16db71672f5ea70e1f
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2022-09-16 05:42:09 +0700

    PUT /songs/id

--- src/services/postgres/SongsService.js
+++ src/services/postgres/SongsService.js
@@ -9,7 +9,7 @@ class SongsService {
     this._pool = new Pool();
   }
 
-  async addSong({ title, year, performer, genre, duration, albumId }) {
+  async addSong({ title, year, genre, performer, duration, albumId }) {
     const id = "song-" + nanoid(10);
     const createdAt = new Date().toISOString();
     const updatedAt = createdAt;
@@ -59,6 +59,20 @@ class SongsService {
     }
 
     return result.rows.map(mapDBToModel)[0];
+  }
+
+  async editSongById(id, { title, year, genre, performer, duration, albumId }) {
+    const updatedAt = new Date().toISOString();
+
+    const query = {
+      text: "UPDATE songs SET title = $1, year = $2, genre = $3, performer = $4, duration = $5, album_id = $6, updated_at = $7 WHERE id = $8 RETURNING id",
+      values: [title, year, genre, performer, duration, albumId, updatedAt, id],
+    };
+
+    const result = await this._pool.query(query);
+    if (!result.rows.length) {
+      throw new NotFoundError("Gagal memperbarui lagu. Id tidak ditemukan");
+    }
   }
 }
 
