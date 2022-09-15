4a186785ecd8ef83770f88204510bb2550f04c53
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2022-09-15 20:47:10 +0700

    GET /albums/id

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -1,9 +1,12 @@
+const ClientError = require("../../exceptions/ClientError");
+
 class AlbumsHandler {
   constructor(service, validator) {
     this._service = service;
     this._validator = validator;
 
     this.postAlbumHandler = this.postAlbumHandler.bind(this);
+    this.getAlbumByIdHandler = this.getAlbumByIdHandler.bind(this);
   }
 
   async postAlbumHandler(request, h) {
@@ -29,6 +32,40 @@ class AlbumsHandler {
         message: error.message,
       });
       response.code(error.statusCode);
+      return response;
+    }
+  }
+
+  async getAlbumByIdHandler(request, h) {
+    try {
+      const { id } = request.params;
+      const album = await this._service.getAlbumById(id);
+
+      return {
+        status: "success",
+        data: {
+          album,
+        },
+      };
+    } catch (error) {
+      if (error instanceof ClientError) {
+        const response = h.response({
+          status: "fail",
+          message: error.message,
+        });
+        response.code(error.statusCode);
+
+        return response;
+      }
+
+      // Server ERROR!
+      const response = h.response({
+        status: "error",
+        message: "Maaf, terjadi kegagalan pada server kami.",
+      });
+      response.code(500);
+      console.error(error);
+
       return response;
     }
   }
