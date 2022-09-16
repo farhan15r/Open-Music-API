f243e5575b34d0897051566efceb145bba341d00
Farhan Ramadhan <nak.rokesselalu@gmail.com>
2022-09-16 07:11:46 +0700

    songs in album

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -32,11 +32,21 @@ class AlbumsService {
       text: "SELECT id, name, year FROM albums WHERE id = $1",
       values: [id],
     };
+
     const result = await this._pool.query(query);
 
     if (!result.rows.length) {
       throw new NotFoundError("Catatan tidak ditemukan");
     }
+
+    const songsQuery = {
+      text: "SELECT id, title, performer FROM songs WHERE album_id = $1",
+      values: [id],
+    };
+
+    const songResult = await this._pool.query(songsQuery);
+
+    result.rows[0].songs = songResult.rows;
 
     return result.rows[0];
   }
